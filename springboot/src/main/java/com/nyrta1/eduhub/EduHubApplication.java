f9316dad37c14343a75f486806bb3d610a3bbd7e
nyrta1 <nurtai_2005@mail.ru>
2024-03-14 20:03:25 +0500

    build: create the spring boot backend app

--- springboot/src/main/java/com/nyrta1/eduhub/EduHubApplication.java
+++ springboot/src/main/java/com/nyrta1/eduhub/EduHubApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EduhubApplication {
+public class EduHubApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EduhubApplication.class, args);
+		SpringApplication.run(EduHubApplication.class, args);
 	}
 
 }
