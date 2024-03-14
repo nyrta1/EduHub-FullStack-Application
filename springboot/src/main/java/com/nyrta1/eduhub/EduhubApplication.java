5d0c8b1cb84cbecabe2cdfd1b28365f440ec9a47
nyrta1 <nurtai_2005@mail.ru>
2024-03-14 18:15:12 +0500

    build: created spring boot

---
+++ springboot/src/main/java/com/nyrta1/eduhub/EduhubApplication.java
@@ -0,0 +1,13 @@
+package com.nyrta1.eduhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduhubApplication.class, args);
+	}
+
+}
